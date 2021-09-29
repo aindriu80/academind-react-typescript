9976376362cdc45f4f9ad946dac25a1f18517ab0
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-09-29 12:32:01 +0100

    Basic types with typescript

---
+++ basics.ts
@@ -0,0 +1,17 @@
+// Primatives: numbers, strings, booleans
+// More Complex types: arrays, objects
+// Function types, paramaters
+
+// Primitives
+
+let age: number = 25
+
+age = 12
+
+let userName: string
+
+userName = 'Aindriú'
+
+let isInstructor: boolean
+
+isInstructor = true
